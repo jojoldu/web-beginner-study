6b489f4d29e838f9d98653cae893aedbb72ec895
jojoldu <jojoldu@gmail.com>
2016-01-09 00:24:47 +0900

    함수 간략 소개

---
+++ week1/function.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jojoldu@gmail.com on 2016-01-08.
+ */
+
+ var a=10;
+ var b=5;
+
+ function sum(num1, num2){
+ 	var result = num1+num2;
+ 	return result;
+ };
+
+ console.log(sum(a,b));
+ console.log(sum(5,1));
