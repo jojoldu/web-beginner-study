e120a7cc2fc977a6ab965679be4e29a6a42bfcd5
jojoldu <jojoldu@gmail.com>
2016-01-08 23:29:41 +0900

    호이스팅 문제 추가

---
+++ week1/hoisting.js
@@ -0,0 +1,29 @@
+/**
+ * Created by jojoldu@zuminternet.com on 2016-01-08.
+ */
+
+//아래의 코드들의 결과를 예상해보시오.
+x = 5;
+console.log(x);
+var x;
+
+
+var m = 5;
+var n;
+console.log(m+n);
+n=7;
+
+
+
+var a = 5;
+console.log(a+b);
+var b = 7;
+
+
+
+console.log(sum(4));
+var sum = 0;
+function sum(num){
+	return num+num;
+};
+console.log(sum);
--- week1/var.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by jojoldu@zuminternet.com on 2016-01-08.
- */
-
-var a = 'a';
