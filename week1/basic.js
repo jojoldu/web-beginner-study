8c56adafae659aa13ffe5c5af094bf9e323def6f
jojoldu <jojoldu@gmail.com>
2016-01-09 00:24:30 +0900

    자바스크립트 리터럴, var 소개

---
+++ week1/basic.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jojoldu@gmail.com on 2016-01-08.
+ */
+
+/*
+	자바스크립트에서 기본 타입(primitive type)은 총 5가지이다.
+	문자열, 숫자, boolean, null, undefined
+
+	* 문자 타입은 없다.
+*/
+
+//모든 변수는 var 로 선언 가능하다.
+var a = 'a';
+var b = "b";
+var c = 0;
+var d = false;
+var e = null;
+var f = undefined;
+
+
+//약한 타입의 언어
+var str = 'aa';
+var num = 0;
+console.log(str+num);
